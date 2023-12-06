17e370f0d61a20866ebcc9bae701b5f90056532c
VinothL123 <150883370+VinothL123@users.noreply.github.com>
2023-12-06 10:30:52 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit
 streamlit.title('My Parents New Healthy Diner checks')
 
-streamlit.header('Breakfast Menu')
+streamlit.header('Breakfast Menu')'
 streamlit.text('Hello')
 streamlit.header('Breakfast Menu')
 streamlit.text(' 🥣Omega 3 & Blueberry Oatmeal')
